e734573d8e0d04b0920a97cf076ee73b6ae5e332
Admin <admin@Admins-MacBook-Pro.local>
2021-11-30 23:28:24 +0530

    Asyncstorage install issue resolved

--- src/utils/Utility.js
+++ src/utils/Utility.js
@@ -1,5 +1,5 @@
 import Toast from 'react-native-simple-toast';
-import AsyncStorage from '@react-native-community/async-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import {Constants} from '../res/index'; 
 
 /**
